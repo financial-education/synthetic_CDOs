4656280381f1ab8dab61a3d2c94e95936dc94eeb
Matt Brigida <mdbrigida@gmail.com>
2020-02-24 17:41:44 -0500

    changed affil

--- beamer_presentation/auto/scdo_presentation.el
+++ beamer_presentation/auto/scdo_presentation.el
@@ -1,22 +1,41 @@
-(TeX-add-style-hook "scdo_presentation"
+(TeX-add-style-hook
+ "scdo_presentation"
  (lambda ()
-    (LaTeX-add-labels
-     "fig:1")
-    (TeX-run-style-hooks
-     "tikz"
-     "bm"
-     "fancyvrb"
-     "xcolor"
-     "lmodern"
-     "graphicx"
-     "makeidx"
-     "amssymb"
-     "amsfonts"
-     "amsmath"
-     "setspace"
-     ""
-     "latex2e"
-     "beamer10"
-     "beamer"
-     "xcolor=dvipsnames")))
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "xcolor=dvipsnames")))
+   (add-to-list 'LaTeX-verbatim-environments-local "Verbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "Verbatim*")
+   (add-to-list 'LaTeX-verbatim-environments-local "BVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "BVerbatim*")
+   (add-to-list 'LaTeX-verbatim-environments-local "LVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "LVerbatim*")
+   (add-to-list 'LaTeX-verbatim-environments-local "SaveVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "VerbatimOut")
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "Verb")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "setspace"
+    "amsmath"
+    "amsfonts"
+    "amssymb"
+    "makeidx"
+    "graphicx"
+    "lmodern"
+    "xcolor"
+    "fancyvrb"
+    "bm"
+    "tikz")
+   (LaTeX-add-labels
+    "fig:1"))
+ :latex)
 
